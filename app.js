927d79402104abc9c557e97d07d8ee8308f57315
Ninzato <ridzakalimanto@gmail.com>
2024-09-30 19:45:40 +0700

    chore: initial project setup

---
+++ app.js
@@ -0,0 +1,21 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const router = require("./routes");
+
+app.use(
+  cors({
+    origin: "*",
+    allowedHeaders: ["Authorization", "Content-Type"],
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
+
+module.exports = app;
