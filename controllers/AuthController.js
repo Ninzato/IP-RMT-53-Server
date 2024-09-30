4eba4be65840daedd84b63635696588b0d5ff8b7
Ninzato <ridzakalimanto@gmail.com>
2024-10-01 01:56:37 +0700

    feat: add login feature

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,5 @@
+const { verifyPassword } = require("../helpers/bcryptjs");
+const { generateToken } = require("../helpers/jwt");
 const { User } = require("../models");
 
 class AuthController {
@@ -14,6 +16,27 @@ class AuthController {
       delete userWithoutPassword.password;
 
       res.status(201).json(userWithoutPassword);
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  static async login(req, res, next) {
+    const { email, password } = req.body;
+    try {
+      if (!email)
+        throw { name: "Bad Request", message: "Please input your email" };
+      if (!password)
+        throw { name: "Bad Request", message: "Please input your password" };
+
+      const user = await User.findOne({ where: { email } });
+
+      if (!user || !verifyPassword(password, user.password))
+        throw { name: "Unauthorized" };
+
+      const access_token = generateToken(user);
+
+      res.status(200).json({ access_token });
     } catch (err) {
       next(err);
     }
--- routes/AuthRouters.js
+++ routes/AuthRouters.js
@@ -3,6 +3,6 @@ const AuthController = require("../controllers/AuthController");
 const router = require("express").Router();
 
 router.post("/register", AuthController.register);
-router.post("/login");
+router.post("/login", AuthController.login);
 
 module.exports = router;
