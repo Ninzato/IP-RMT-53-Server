c5690a799e1e9365789cbfdcb6a97ab1e91d1561
Ninzato <ridzakalimanto@gmail.com>
2024-09-30 19:48:00 +0700

    chore: add migration and model file for Users table

---
+++ models/user.js
@@ -0,0 +1,62 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        unique: {
+          msg: "Email has already been used!",
+        },
+        validate: {
+          notNull: {
+            msg: "Email is required!",
+          },
+          notEmpty: {
+            msg: "Email is required!",
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          notNull: {
+            msg: "Password is required!",
+          },
+          notEmpty: {
+            msg: "Password is required!",
+          },
+        },
+      },
+      username: {
+        type: DataTypes.STRING,
+        unique: {
+          msg: "Username has already been taken!",
+        },
+        validate: {
+          notNull: {
+            msg: "Username is required!",
+          },
+          notEmpty: {
+            msg: "Username is required!",
+          },
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  return User;
+};
