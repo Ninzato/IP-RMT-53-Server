b4f8aa2b68bd0fc9655a581cf4bd061a0c2734d7
Ninzato <ridzakalimanto@gmail.com>
2024-10-04 05:44:37 +0700

    refactor: fix specialSeed

--- specialSeed.js
+++ specialSeed.js
@@ -1,13 +1,30 @@
+const { Sequelize } = require("sequelize");
 const { Skill } = require("./models");
-
 const data = require("./data/skills.json").map((el) => {
   el.createdAt = new Date();
   el.updatedAt = new Date();
   return el;
 });
 
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  logging: false,
+});
+
 const fillItUp = async () => {
-  await Skill.bulkCreate(data);
+  try {
+    await sequelize.authenticate();
+    console.log("Connection success.");
+
+    await Skill.sync();
+
+    await Skill.bulkCreate(data);
+    console.log("Skills have been seeded successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  } finally {
+    await sequelize.close();
+  }
 };
 
 fillItUp();
